4687167b2eb91f6f857fb12471ae28bc0546e4d8
LiubovPil <alexandraelle95@gmail.com>
2022-09-01 15:23:08 +0300

    Adding Timer

--- Ship/Assets/Scripts/Ship/ShipSpawner.cs
+++ Ship/Assets/Scripts/Ship/ShipSpawner.cs
@@ -1,17 +1,32 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ShipTest.Utility;
 
 namespace ShipTest.Ship
 {
     public class ShipSpawner : MonoBehaviour
     {
-        [SerializeField] 
-        private List<ShipData> _ships;
+        [SerializeField]
+        private List<ShipData> _listOfShips;
 
-        private void SpawnShip()
+        private Timer _spawnShipTimer;
+
+        private int _maxNumShip;
+        private int _countShip;
+
+        private void Awake()
+        {
+            _spawnShipTimer = GetComponent<Timer>();
+        }
+        private void Update()
         {
             
         }
+        private void SpawnShip()
+        {
+            int indexShip = Random.Range(0, _listOfShips.Count);
+
+        }
+            
     }
 }
--- Ship/Assets/Scripts/Utility/Timer.cs
+++ Ship/Assets/Scripts/Utility/Timer.cs
@@ -1,21 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace ShipTest.Timer
+namespace ShipTest.Utility
 {
-    public class Timer : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+	public class Timer : MonoBehaviour
+	{
+		private float _duration;
+		private float _currentTime;
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    }
+		private bool _started;
+		private bool _finished;
+
+		public float Duration
+		{
+			get { return _duration; }
+			set { _duration = value; }
+		}
+		public bool Started
+		{
+			get { return _started; }
+		}
+		public bool Finished
+		{
+			get { return _finished; }
+		}
+		private void Update()
+		{
+			if (_started && !_finished)
+			{
+				_currentTime += Time.deltaTime;
+				if (_currentTime >= _duration)
+				{
+					_finished = true;
+					_started = false;
+				}
+			}
+		}
+		public void Run()
+		{
+			if (_duration > 0)
+			{
+				_started = true;
+				_finished = false;
+
+				_currentTime = 0;
+			}
+		}
+	}
 }
